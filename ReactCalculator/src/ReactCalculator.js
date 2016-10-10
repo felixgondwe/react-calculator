e1853b8fa57cc7390be5153e5d28d4a40ada30a2
felixgondwe <zungwala@gmail.com>
2016-10-09 22:07:04 -0400

    UI: buttons

--- ReactCalculator/src/ReactCalculator.js
+++ ReactCalculator/src/ReactCalculator.js
@@ -17,11 +17,22 @@ const inputButtons = [
 ];
 
 class ReactCalculator extends Component {
+    
+   constructor(props){
+        super(props);
+        this.state = {
+          //  previousInputValue: 0,
+            inputValue: 0
+          //  selectedSymbol: null
+        }
+    }
 
     render() {
         return (
             <View style={Style.rootContainer}>
-                <View style={Style.displayContainer}></View>
+                <View style={Style.displayContainer}>
+                    <Text style={Style.displayText}>{this.state.inputValue} </Text>
+                </View>
                 <View style={Style.inputContainer}>
                     {this._renderInputButtons()}
                 </View>
@@ -43,14 +54,63 @@ class ReactCalculator extends Component {
                 let input = row[i];
 
                 inputRow.push(
-                    <InputButton value={input} key={r + "-" + i} />
+                    <InputButton
+                value={input}
+                onPress={this._onInputButtonPressed.bind(this, input)}
+                key={r + "-" + i}/>
                 );
             }
 
             views.push(<View style={Style.inputRow} key={"row-" + r}>{inputRow}</View>)
         }
-
         return views;
+    }
+   /* _onInputButtonPressed(input) {
+        alert(input)
+    }*/
+    _onInputButtonPressed(input){
+        alert(input)
+       /* switch (typeof input) {
+            case 'number':
+                return this._handleNumberInput(input)
+            case 'string':
+                return this._handleStringInput(input)
+        } */
+        
+    } 
+    _handleNumberInput(num){
+        let inputValue = (this.state.inputValue *10) + num;
+        this.setState({inputValue: inputValue})
+    }
+    _handleStringInput(str){
+        switch(str){
+            case '/':
+            case '*':
+            case '+':
+            case '-':
+                this.setState({
+                    selectedSymbol:str,
+                    previousInputValue: this.state.inputValue,
+                    inputValue: 0
+
+                });
+                break;
+            case '=':
+                let symbol = this.state.selectedSymbol,
+                    inputValue = this.state.inputValue,
+                    previousInputValue = this.state.previousInputValue;
+
+                if (!symbol) {
+                    return;
+                }
+
+                this.setState({
+                    previousInputValue: 0,
+                    inputValue: eval(previousInputValue + symbol + inputValue),
+                    selectedSymbol: null
+                });
+                break;
+        }
     }
 }
 
